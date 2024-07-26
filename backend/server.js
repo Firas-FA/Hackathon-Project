353421a9939fbdaf3c5f46aeec137a2b68eb2ba5
Firas-FH <firas2001hssan22@gmail.com>
2024-07-26 10:41:48 +0300

    edit files and add contract not completed

--- backend/server.js
+++ backend/server.js
@@ -28,9 +28,6 @@ app.use(cors());
 
 
 
-
-
-
 app.use("/user", userRegisterRoute);
 app.use("/user", userLoginRoute);
 app.use("/user", getAllUserRoute)
@@ -38,43 +35,7 @@ app.use("/user", getAllUserRoute)
 app.use("/owner", createOwnerRoute)
 app.use("/owner", getAllOwnerRoute)
 
-
-
-
-
-
-// Route to create a new RentContract
-app.post("/rentcontracts", async (req, res) => {
-  try {
-    const { id, id_apartment, user_name, Rental_period, total_rental } = req.body;
-
-    // Ensure the referenced apartment exists
-    const apartment = await Apartment.findById(id_apartment);
-    if (!apartment) {
-      return res.status(400).json({ error: "Apartment not found" });
-    }
-
-    // Create the new RentContract
-    const newRentContract = new RentContract({
-      id,
-      id_apartment,
-      user_name,
-      Rental_period,
-      total_rental,
-    });
-
-    // Save the RentContract
-    const savedRentContract = await newRentContract.save();
-
-    // Return the saved RentContract
-    res.status(201).json(savedRentContract);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-});
-
-
-
+ 
 
 connectDB().then(() => {
   //? Starting the server after successful database connection
@@ -83,4 +44,52 @@ connectDB().then(() => {
   });
 }
 )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Route to create a new RentContract
+// app.post("/rentcontracts", async (req, res) => {
+//   try {
+//     const { id, id_apartment, user_name, Rental_period, total_rental } = req.body;
+
+//     // Ensure the referenced apartment exists
+//     const apartment = await Apartment.findById(id_apartment);
+//     if (!apartment) {
+//       return res.status(400).json({ error: "Apartment not found" });
+//     }
+
+//     // Create the new RentContract
+//     const newRentContract = new RentContract({
+//       id,
+//       id_apartment,
+//       user_name,
+//       Rental_period,
+//       total_rental,
+//     });
+
+//     // Save the RentContract
+//     const savedRentContract = await newRentContract.save();
+
+//     // Return the saved RentContract
+//     res.status(201).json(savedRentContract);
+//   } catch (error) {
+//     res.status(400).json({ error: error.message });
+//   }
+// });
 
