363032a62019ee55f9dda778a2b23f460c95bc86
Noura-Bdr-Aldeen <nourabdraldeen@gmail.com>
2024-07-26 17:35:52 +0300

    card

--- frontend/src/Components/cardhouse/houseCard.js
+++ frontend/src/Components/cardhouse/houseCard.js
@@ -6,22 +6,18 @@ function HouseCard ({appartmentLocation,img,appartmentarea,countRoom,FloorHight,
  
   return (
     <><div className="container">
-    <div className="row">
-        <div className="col-lg-4 mb-4">
             <div className="card custom-card">
                 <img src={img} alt="" className="card-img-top custom-card-image"/>
                 <div className="card-body">
                     <h5 className="card-title custom-card-title">Location: {appartmentLocation}</h5>
                     <p className="card-text custom-card-detail">Block Number: {blockNumber}</p>
                     <p className="card-text custom-card-detail">Floor Height: {FloorHight}</p>
-                    <p className="card-text custom-card-detail">Count Room: {countRoom}</p>
+                    <div className="card-text custom-card-detail d-flex"><img width="30" height="30" src="https://img.icons8.com/ultraviolet/40/room.png" alt="room"/>:   {countRoom}</div>
                     <p className="card-text custom-card-detail">Price: {apartmentprice}</p>
                     <p className="card-text custom-card-detail">Area: {appartmentarea}</p>
                     <Link className='btn btn-info custom-link' to="/details">Details</Link>
                 </div>
             </div>
-        </div>
-    </div>
 </div>
 
     </>
