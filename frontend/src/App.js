6682faaba783639b685d06497d1a9950ea7c48ac
Firas-FH <firas2001hssan22@gmail.com>
2024-07-27 09:53:40 +0300

    uplode

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ import LandingPage from './Pages/Landing Page/LandingPage';
 import Login from './Pages/Login/Login';
 import Signup from './Pages/SignuUp/Signup';
 import Details from './Pages/Details/Details';
-import Sidebar from './Pages/Admin Page/AdminPage.jsx';
+// import Sidebar from './Pages/Admin Page/AdminPage.jsx';
 
 
 
@@ -16,7 +16,7 @@ function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/home" element={<HomePage />} />
         <Route path="/details" element={<Details />} />
-        <Route path="/Admin" element={<Sidebar />} />
+        {/* <Route path="/Admin" element={<Sidebar />} /> */}
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
       </Routes>
